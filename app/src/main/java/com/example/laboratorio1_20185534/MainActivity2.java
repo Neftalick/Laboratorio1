e45e799e3dca325098eabc171a0b834a7bcd6205
AgustinVizcarra <a20181454@pucp.edu.pe>
2022-09-02 09:53:40 -0500

    Implementacion de la logica de Juego

--- app/src/main/java/com/example/laboratorio1_20185534/MainActivity2.java
+++ app/src/main/java/com/example/laboratorio1_20185534/MainActivity2.java
@@ -27,55 +27,55 @@ public class MainActivity2 extends AppCompatActivity {
         Button btn9 = findViewById(R.id.btn9);
         btn1.setOnClickListener(view -> {
             String label = btn1.getText().toString();
-            if (!(label.equals("O") || label.equals("X"))){
+            if (!(label.equals("O") || label.equals("X"))) {
                 CambiarEstado(btn1);
             }
         });
         btn2.setOnClickListener(view -> {
             String label = btn2.getText().toString();
-            if (!(label.equals("O") || label.equals("X"))){
+            if (!(label.equals("O") || label.equals("X"))) {
                 CambiarEstado(btn2);
             }
         });
         btn3.setOnClickListener(view -> {
             String label = btn3.getText().toString();
-            if (!(label.equals("O") || label.equals("X"))){
+            if (!(label.equals("O") || label.equals("X"))) {
                 CambiarEstado(btn3);
             }
         });
         btn4.setOnClickListener(view -> {
             String label = btn4.getText().toString();
-            if (!(label.equals("O") || label.equals("X"))){
+            if (!(label.equals("O") || label.equals("X"))) {
                 CambiarEstado(btn4);
             }
         });
         btn5.setOnClickListener(view -> {
             String label = btn5.getText().toString();
-            if (!(label.equals("O") || label.equals("X"))){
+            if (!(label.equals("O") || label.equals("X"))) {
                 CambiarEstado(btn5);
             }
         });
         btn6.setOnClickListener(view -> {
             String label = btn6.getText().toString();
-            if (!(label.equals("O") || label.equals("X"))){
+            if (!(label.equals("O") || label.equals("X"))) {
                 CambiarEstado(btn6);
             }
         });
         btn7.setOnClickListener(view -> {
             String label = btn7.getText().toString();
-            if (!(label.equals("O") || label.equals("X"))){
+            if (!(label.equals("O") || label.equals("X"))) {
                 CambiarEstado(btn7);
             }
         });
         btn8.setOnClickListener(view -> {
             String label = btn8.getText().toString();
-            if (!(label.equals("O") || label.equals("X"))){
+            if (!(label.equals("O") || label.equals("X"))) {
                 CambiarEstado(btn8);
             }
         });
         btn9.setOnClickListener(view -> {
             String label = btn9.getText().toString();
-            if (!(label.equals("O") || label.equals("X"))){
+            if (!(label.equals("O") || label.equals("X"))) {
                 CambiarEstado(btn9);
             }
         });
@@ -83,28 +83,76 @@ public class MainActivity2 extends AppCompatActivity {
 
     }
 
-    public void CambiarEstado(View view){
+    public void CambiarEstado(View view) {
         Button button = (Button) view;
-        if(primero){
+        if (primero) {
             //inicio primero el juego
             button.setText("O");
             primero = false;
-        }else{
+        } else {
             //le toca jugar al segundo
             button.setText("X");
             primero = true;
         }
     }
 
-    public void ReiniciarJuego(View view){
+    public void ReiniciarJuego(View view) {
         //Resetear todo el juego
         TableLayout tableLayout = (TableLayout) findViewById(R.id.tableLayout);
-        for(int i =0; i <=2;i++){
+        for (int i = 0; i <= 2; i++) {
             TableRow tr = (TableRow) tableLayout.getChildAt(i);
-            for(int j=0;j<=2;j++){
+            for (int j = 0; j <= 2; j++) {
                 Button button = (Button) tr.getChildAt(j);
                 button.setText("-");
             }
         }
+    }
+    //falta llamarlo en los listernes de los botones
+    public boolean ganadorFila(TableLayout tl, Button btn) {
+        //Hallamos en qué fila se encuentra el button
+        int idButton = btn.getId();
+        int fila;
+        if(idButton%3==0){
+             fila = idButton/3-1;
+        }else{
+             fila = (idButton-(idButton%3))/3;
+        }
+        boolean ganadorFila = true;
+        TableRow tr = (TableRow) tl.getChildAt(fila);
+        for (int j = 0; j <= 1; j++) {
+            //basta que dos sean distintos para que no haya ganador dentro de una fila
+            Button btnAct = (Button) tr.getChildAt(j);
+            Button btnPost = (Button) tr.getChildAt(j + 1);
+            if (!(btnAct.getText().toString().equalsIgnoreCase(btnPost.getText().toString()))) {
+                ganadorFila = false;
+                break;
+            }
+        }
+        return ganadorFila;
+    }
+    //falta llamarlo en los listernes de los botones
+    public boolean ganadorColumna(TableLayout tl, Button btn) {
+        int idButton = btn.getId();
+        int columna = idButton%3!=0?(idButton%3-1):2;
+        boolean ganadorColumna =true;
+        for (int j=0;j <=1;j++){
+            TableRow tr1 = (TableRow) tl.getChildAt(j);
+            TableRow tr2 = (TableRow) tl.getChildAt(j+1);
+            Button btnAct = (Button) tr1.getChildAt(columna);
+            Button btnPost = (Button)  tr2.getChildAt(columna);
+            if(!(btnAct.getText().toString().equalsIgnoreCase(btnPost.getText().toString()))){
+                //basta que uno sea distinto
+                ganadorColumna = true;
+                break;
+            }
+        }
+        return ganadorColumna;
+    }
+
+    public boolean ganadorDiagonal() {
+        for (int i = 0; i <= 2; i++) {
+
+        }
+        return true;
     }
 }
