08f9e2029ef21eda7e2290561169b15a1c0f2dcf
AgustinVizcarra <a20181454@pucp.edu.pe>
2022-09-02 08:38:07 -0500

    commit activity 2

--- app/src/main/java/com/example/laboratorio1_20185534/MainActivity2.java
+++ app/src/main/java/com/example/laboratorio1_20185534/MainActivity2.java
@@ -3,12 +3,34 @@ package com.example.laboratorio1_20185534;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    boolean primero = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void CambiarEstado(View view){
+        Button button = (Button) view;
+        if(primero){
+            //inicio primero el juego
+            button.setText("O");
+            primero = false;
+        }else{
+            //le toca jugar al segundo
+            button.setText("X");
+            primero = true;
+        }
+    }
+
+    public void ReiniciarJuego(View view){
+        //id
+
     }
 }
